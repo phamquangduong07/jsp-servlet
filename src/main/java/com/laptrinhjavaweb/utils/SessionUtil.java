215e1de7a7030b8b93a2a702d0b8bd81e228a59e
phamquangduong07 <phamquangduong07@gmail.com>
2023-08-16 14:40:42 +0700

    Bai 34

--- src/main/java/com/laptrinhjavaweb/utils/SessionUtil.java
+++ src/main/java/com/laptrinhjavaweb/utils/SessionUtil.java
@@ -1,5 +1,27 @@
 package com.laptrinhjavaweb.utils;
 
-public class SessionUtil {
+import javax.servlet.http.HttpServletRequest;
 
+public class SessionUtil {
+    private static SessionUtil sessionUtil = null;
+
+    public static SessionUtil getInstance() {
+        if (sessionUtil == null) {
+            sessionUtil = new SessionUtil();
+        }
+        return sessionUtil;
+    }
+
+    public void putValue(HttpServletRequest request, String key, Object value) {
+        request.getSession().setAttribute(key, value);
+    }
+
+    public Object getValue(HttpServletRequest request, String key) {
+        request.getSession().getAttribute(key);
+        return null;
+    }
+
+    public void removeValue(HttpServletRequest request, String key) {
+        request.getSession().removeAttribute(key);
+    }
 }
