05fdaf026de23113133be3d1b6e76d57062dd765
phamquangduong07 <phamquangduong07@gmail.com>
2023-08-16 16:41:40 +0700

    Bai 35

--- src/main/java/com/laptrinhjavaweb/controller/web/HomeController.java
+++ src/main/java/com/laptrinhjavaweb/controller/web/HomeController.java
@@ -15,53 +15,57 @@ import com.laptrinhjavaweb.service.ICategoryService;
 import com.laptrinhjavaweb.service.INewService;
 import com.laptrinhjavaweb.service.IUserService;
 import com.laptrinhjavaweb.utils.FormUtil;
+import com.laptrinhjavaweb.utils.SessionUtil;
 
-@WebServlet(urlPatterns = { "/trang-chu", "/dang-nhap" })
+@WebServlet(urlPatterns = {"/trang-chu", "/dang-nhap", "/thoat"})
 public class HomeController extends HttpServlet {
 
-	@Inject
-	private ICategoryService categoryService;
-	@Inject
-	INewService newService;
-	@Inject
-	private IUserService userService;
-	private static final long serialVersionUID = 2686801510274002166L;
+    @Inject
+    private ICategoryService categoryService;
+    @Inject
+    INewService newService;
+    @Inject
+    private IUserService userService;
+    private static final long serialVersionUID = 2686801510274002166L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String action = request.getParameter("action");
-		if (action != null && action.equals("login")) {
-			RequestDispatcher rd = request.getRequestDispatcher("/views/login.jsp");
-			rd.forward(request, response);
-		} else if (action != null && action.equals("logout")) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action != null && action.equals("login")) {
+            RequestDispatcher rd = request.getRequestDispatcher("/views/login.jsp");
+            rd.forward(request, response);
+        } else if (action != null && action.equals("logout")) {
+            SessionUtil.getInstance().removeValue(request, "USERMODEL");
+            response.sendRedirect(request.getContextPath() + "/trang-chu");
+        } else {
+            request.setAttribute("categories", categoryService.findAll());
+            RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
+            rd.forward(request, response);
+        }
 
-		} else {
-			request.setAttribute("categories", categoryService.findAll());
-			RequestDispatcher rd = request.getRequestDispatcher("/views/web/home.jsp");
-			rd.forward(request, response);
-		}
+    }
 
-	}
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action != null && action.equals("login")) {
+            UserModel model = FormUtil.toModel(UserModel.class, request);
+            model = userService.findByUserNameAndPasswordAndStatus(model.getUserName(), model.getPassword(), 1);
+            if (model != null) {
+                SessionUtil.getInstance().putValue(request, "USERMODEL", model);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String action = request.getParameter("action");
-		if (action != null && action.equals("login")) {
-			UserModel model = FormUtil.toModel(UserModel.class, request);
-			model = userService.findByUserNameAndPasswordAndStatus(model.getUserName(), model.getPassword(), 1);
-			if (model != null) {
-				if (model.getRole().getCode().equals("USER")) {
-					response.sendRedirect(request.getContextPath() + "/trang-chu");
+                if (model.getRole().getCode().equals("USER")) {
+                    response.sendRedirect(request.getContextPath() + "/trang-chu");
 
-				} else if (model.getRole().getCode().equals("ADMIN")) {
-					response.sendRedirect(request.getContextPath() + "/admin-home");
+                } else if (model.getRole().getCode().equals("ADMIN")) {
+                    response.sendRedirect(request.getContextPath() + "/admin-home");
+                //sendRedirect: chuyển hướng trang
+                } else {
+                    response.sendRedirect(request.getContextPath() + "/dang-nhap?action=login");
+                }
+            }
 
-				} else {
-					response.sendRedirect(request.getContextPath() + "/dang-nhap?action=login");
-				}
-			}
-
-		}
-	}
+        }
+    }
 
 }
